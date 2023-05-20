625e431f310250abc08ae7a942cc56a7ceff0f9e
hyeokjin-eun <gurwls2399@gmail.com>
2023-05-21 01:53:35 +0900

    feat : jenkins first

---
+++ src/main/java/com/example/jenkins/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(@RequestParam String name) {
+        return "Hello" + " " + name;
+    }
+}
